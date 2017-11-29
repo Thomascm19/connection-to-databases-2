d0b1513ca776046a7fc981e01ef2b48677b893f8
thomas.caycedo <thomascaycedo19@outlook.com>
2017-11-28 23:31:20 -0500

    Se añaden los archivos

---
+++ Data/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Cliente
+    {
+        public string CC { get; set; }
+        public string Nombre { get; set; }
+        public string Tel { get; set; }
+    }
+}
---
+++ Data/ClienteData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class ClienteData
+    {
+        public void CrearCliente(Cliente unCliente)
+        {
+            SqlConnection con = new SqlConnection();
+         
+            con.ConnectionString = "Data Source=THOMAS-CM-PC\\SQLEXPRESS;Initial Catalog=Web;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            con.Open();
+            SqlCommand com = new SqlCommand();
+            com.Connection = con;
+            com.CommandText = "INSERT INTO Clientes (Nombre,CC,Tel) values('"+unCliente.Nombre+"','"+unCliente.CC+"','"+unCliente.Tel+"')";
+            com.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
